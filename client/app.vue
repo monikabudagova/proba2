9ba1f56e9ab07476d35458024f653b06d7bce734
Monika <budagova.monika5@yandex.ru>
2024-09-26 18:48:38 +0300

    update

---
+++ client/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Navbar />
+    <NuxtPage />
+  </div>
+</template>
+
+<style scoped>
+  body{
+    width: 1024px;
+    margin: auto
+}
+</style>
+
+<script setup>
+
+</script>
