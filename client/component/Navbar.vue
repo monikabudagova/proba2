b0003507785d1c25914cac634f2192893b4863e6
Monika <budagova.monika5@yandex.ru>
2024-09-26 20:53:12 +0300

    update

--- client/component/Navbar.vue
+++ client/component/Navbar.vue
@@ -5,7 +5,6 @@
             <li><nuxt-link to="/about">About</nuxt-link></li>
             <li><nuxt-link to="/contact">Contact</nuxt-link></li>
         </ul>
-
     </nav>
 </template>
 
--- client/pages/blog.vue
+++ client/pages/blog.vue
@@ -1,12 +1,25 @@
 <template>
     <h2>блог</h2>
+
     <main>
-        <article>
-            {{ posts }}
+        <article v-for="post in posts" :key="post.id">
+            <h3>{{ posts.title }}</h3>
+            <img :src=base_url+post.img.url alt="обложка статьи">
+            <p>{{ post.desc }}</p>
+            <NuxtLink to="#">подробнее</NuxtLink>
         </article>
     </main>
 </template>
 <script>
-    let api = await $fetch('https;//dummyjson.com/posts')
+    const api = await $fetch('https;//dummyjson.com/posts')
     const posts = api.data
+
+    const base_url = 'http://localhost:1337'
 </script>
+<style scoped>
+main{
+    display: flex;
+    gap: 10px;
+}
+
+</style>
