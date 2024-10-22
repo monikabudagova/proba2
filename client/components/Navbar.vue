4ed26482627d0485332ae8a5c6f2304b2e1492d3
Monika <budagova.monika5@yandex.ru>
2024-10-22 21:02:47 +0300

    update

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -1,4 +1,5 @@
 <template>
+    {{ route }}
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="http://localhost:3000/" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -40,13 +41,18 @@
         <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li><NuxtLink to="/" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</NuxtLink></li>
             <li><NuxtLink to="/blog" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</NuxtLink></li>
-            <li><NuxtLink to="/contact"  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</NuxtLink></li>
+            <li><NuxtLink to="/contact"  :class="(route.path === '/contact')">Services</NuxtLink></li>
         </ul>
         </div>
     </div>
     </nav>
 </template>
+<script setup>
+const route = useRoute()
+const path = route.path.split
+const activeClass = ref('text-blue-5000')
 
+</script>
 <style scoped>
     ul {
         background-color: #F4DBDB;
--- client/pages/blog.vue
+++ client/pages/blog.vue
@@ -20,7 +20,7 @@
           </div>
       </article>
     </div>
-    <button type="button" class="w-full text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Смотреть еще</button>
+    <button @click="loadMore" type="button" class="w-full text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Смотреть еще</button>
   </main>
 
 </template>
@@ -30,6 +30,8 @@ const api = await $fetch('http://localhost:1337/api/posts?pagination[pageSize]=3
 const posts = api.data
 
 const base_url = "http://localhost:1337"
+
+
 </script>
 
 
